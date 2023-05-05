3361e822748c2935fa509edd04c7a3322f3c04e7
Rita Marques <rimarque@c1r16s4.42porto.com>
2023-05-05 18:14:54 +0100

    dj

--- libftx/libft/ft_strjoin.c
+++ libftx/libft/ft_strjoin.c
@@ -6,11 +6,12 @@
 /*   By: rimarque <rimarque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 18:52:32 by rimarque          #+#    #+#             */
-/*   Updated: 2023/05/04 16:28:21 by rimarque         ###   ########.fr       */
+/*   Updated: 2023/05/05 18:12:28 by rimarque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
+#include "../includes/ft_printf.h"
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
@@ -20,13 +21,20 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	if (!s1 || !s2)
 		return (0);
-	if (!ft_strncmp(s1, s2, 5))
-		s2 = s2 + 5;
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
 	new_s = (char *) malloc((s1_len + s2_len + 1) * sizeof(char));
 	if (!new_s)
 		return (0);
+	if (!ft_strncmp("./", s2, 2))
+	{
+		ft_strlcpy(new_s, s2, (s2_len + 1));
+		return (new_s);
+	}
+	if (!ft_strncmp(s1, s2, 5))
+		s2 = s2 + 5;
+	else if (!ft_strncmp("bin/", s2, 4))
+		s2 = s2 + 4;
 	ft_strlcpy(new_s, s1, (s1_len + 1));
 	ft_strlcpy((new_s + s1_len), s2, (s2_len + 1));
 	return (new_s);
--- src_mandatory/pipex.c
+++ src_mandatory/pipex.c
@@ -6,11 +6,45 @@
 /*   By: rimarque <rimarque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 12:31:10 by rimarque          #+#    #+#             */
-/*   Updated: 2023/05/04 18:45:39 by rimarque         ###   ########.fr       */
+/*   Updated: 2023/05/05 18:13:27 by rimarque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
+
+void	error_management(int result, char *str, int exit_code, int stdout_copy)
+{
+	if (result == -1)
+	{
+			if (!str)
+				ft_printf("pipex: %s\n", strerror(errno));
+			else if (stdout_copy == 0)
+				ft_printf("pipex: %s: %s\n", str, strerror(errno));
+			else
+			{
+				dup2(stdout_copy, STDOUT_FILENO); //change stdout back to stdout
+				if (!ft_strncmp("/bin/", str, 5) || !ft_strncmp("bin/", str, 4))
+					ft_printf("pipex: %s: %s\n", str, strerror(errno));
+				else
+					ft_printf("pipex: %s: command not found\n", str);
+			}
+			ft_printf("exit code: %d\n", exit_code);
+			exit(exit_code);
+	}
+}
+
+/*void	error_management_commands(int result, char *str, int exit_code, int stdout_copy)
+{
+	if (error == -1)
+	{
+		dup2(stdout_copy, STDOUT_FILENO); //change stdout back to stdout
+		if (!ft_strncmp("/bin/", command[0], 5) || !ft_strncmp("bin/", command[0], 4))
+			ft_printf("pipex: %s: %s\n", command[0], strerror(errno));
+		else
+			ft_printf("pipex: %s: command not found\n", command[0]);
+		exit(exit_code); //127
+		}
+}*/
 
 int	main(int argc, char **argv, char **envp) //command[0] = filename; command[last] = NULL;
 {
@@ -28,79 +62,49 @@ int	main(int argc, char **argv, char **envp) //command[0] = filename; command[la
 		ft_printf("pipex: the program must take 4 arguments, no more, no less\n");
 		return (0);
 	}
-	if(pipe(fd) == -1) //ERROR
-		return (1);
+	error_management(pipe(fd), NULL, errno, 0);
+	//if(pipe(fd) == -1)
+	//	return (errno);
 	pid1 = fork();
-	if (pid1 == -1) //ERROR
-		return (2);
+	error_management(pid1, NULL, errno, 0);
+	//if (pid1 == -1)
+	//	return (errno);
 	if (pid1 == 0)
 	{
-		int stdoutCopy = dup(1); //clone stdout to a new descriptor
 		close(fd[0]);			 //close pipe reading fd
-		/*if (access(argv[1], R_OK) == -1) ENTRAR AQUI QUANDO N DEVE
-		{
-			ft_printf("pipex: %s: %s\n", argv[1], strerror(errno));
-			ft_printf("oi\n");
-			return(0);
-		}*/
+		int stdout_copy = dup(1); //clone stdout to a new descriptor
 		file1 = open(argv[1], O_RDONLY);  //open fd to file1
-		if (file1 == -1)
-		{
-			//waitpid(pid2, NULL, 0);
-			dup2(stdoutCopy, STDOUT_FILENO); //change stdout back to stdout
-			ft_printf("pipex: %s: %s\n", argv[1], strerror(errno));
-			//ft_printf("oi\n");
-			//ft_printf("pipex: %s: no sush file or directory\n", argv[1]);
-			exit(3);
-		}
+		error_management(file1, argv[1], 1, 0);
 		dup2(file1, STDIN_FILENO);        //change stdin to file1
 		close(file1);                     //close fd of file1
 		dup2(fd[1], STDOUT_FILENO);       //change stdout to pipe writing fd
 		close(fd[1]);					  //close pipe writing fd
 		command = ft_split(argv[2], ' '); //creating array of command
-		pathname = ft_strjoin("/bin/", command[0]); //creanting path name for executable of comand
+		pathname = ft_strjoin("/bin/", command[0]); //creanting path name for executable of comand //APRENDER A TESTAR UM SCRIPT
 		int error = execve((const char *)pathname, (char **const)command, envp); //executing comand on file1 and puting output on pipe writing fd//LIDAR COM ERROS NO COMANDO
-		if (error == -1)
-		{
-			dup2(stdoutCopy, STDOUT_FILENO); //change stdout back to stdout
-			ft_printf("pipex: %s: %s\n", command[0], strerror(errno));
-			//ft_printf("pipex: %s: command not found\n", command[0]);
-			return(3);
-		}
+		error_management(error, command[0], 127, stdout_copy);
 	}
 	pid2 = fork();
 	if (pid2 == -1)
-		return(4);
+		return(1);
 	if (pid2 == 0)
 	{
-		int stdoutCopy = dup(1); //clone stdout to a new descriptor
 		close(fd[1]);
+		int stdout_copy = dup(1);
 		dup2(fd[0], STDIN_FILENO);
 		close(fd[0]);
-		file2 = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644); //se n existir, cria com permissoes 664: you can read and write; others can only read, se existir apaga;
-		if (file2 == -1)
-		{
-			dup2(stdoutCopy, STDOUT_FILENO); //change stdout back to stdout
-			ft_printf("pipex: %s: %s\n", argv[4], strerror(errno));
-			//ft_printf("pipex: %s: no sush file or directory\n", argv[4]);
-			exit(3);
-		}
+		file2 = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		error_management(file2, argv[4], 1, 0);
 		dup2(file2, STDOUT_FILENO);
 		close(file2);
 		command = ft_split(argv[3], ' ');
-		pathname = ft_strjoin("/bin/", command[0]); //bin ou usr/bin
-		int error = execve((const char *)pathname, (char **const)command, envp); //LIDAR COM ERROS NO COMANDO
-		if (error == -1)
-		{
-			dup2(stdoutCopy, STDOUT_FILENO); //change stdout back to stdout
-			ft_printf("pipex: %s: %s\n", command[0], strerror(errno));
-			//ft_printf("pipex: %s: command not found\n", command[0]);
-			return(5);
-		}
+		pathname = ft_strjoin("/bin/", command[0]);
+		int error = execve((const char *)pathname, (char **const)command, envp);
+		error_management(error, command[0], 127, stdout_copy);
 	}
 	close(fd[0]);
 	close(fd[1]);
 	waitpid(pid1, NULL, 0);
-	waitpid(pid2, &wstatus, 0); //RETORNAR O EXIT STATUS --> VER ISTO errno?
+	waitpid(pid2, &wstatus, 0);
 	return (WEXITSTATUS(wstatus));
 }
